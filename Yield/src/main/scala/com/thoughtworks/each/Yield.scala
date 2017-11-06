9661aedfcd38f76729cb8c07600e2aedeb3c0fd1
Yang Bo <pop.atry@gmail.com>
2017-11-06 18:28:30 +0800

    Improve Scaladoc

--- Yield/src/main/scala/com/thoughtworks/each/Yield.scala
+++ Yield/src/main/scala/com/thoughtworks/each/Yield.scala
@@ -2,9 +2,9 @@ package com.thoughtworks.each
 
 /**
   * @author 杨博 (Yang Bo)
+  * @note We cannot make `Yield` a value class due to https://github.com/scala/bug/issues/10588.
   */
-final case class Yield[Element](element: Element)
-    extends /* AnyVal with */ Continuation.InstructionOps[Yield[Element], Unit] {
+final case class Yield[Element](element: Element) extends Continuation.InstructionOps[Yield[Element], Unit] {
   protected def self: Yield[Element] = this
 }
 
