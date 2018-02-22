6576158885336a350683c66a3c7105a1420053fd
Yang Bo <atryyang@thoughtworks.com>
2018-02-22 13:28:37 +0800

    Support cats.MonadError

--- instructions-CatsFlatMap/src/main/scala/com/thoughtworks/dsl/instructions/CatsFlatMap.scala
+++ instructions-CatsFlatMap/src/main/scala/com/thoughtworks/dsl/instructions/CatsFlatMap.scala
@@ -3,9 +3,12 @@ package com.thoughtworks.dsl.instructions
 import cats.FlatMap
 import com.thoughtworks.dsl.Dsl
 import com.thoughtworks.dsl.Dsl.Instruction
+import cats.MonadError
 
 import scala.language.higherKinds
 import scala.language.implicitConversions
+import scala.util.control.Exception.Catcher
+import scala.util.control.NonFatal
 
 /**
   * @author 杨博 (Yang Bo)
@@ -13,6 +16,22 @@ import scala.language.implicitConversions
 final case class CatsFlatMap[F[_], A](fa: F[A]) extends AnyVal with Instruction[CatsFlatMap[F, A], A]
 
 object CatsFlatMap {
+
+  implicit final class CatsMonadErrorCatchOps[F[_], A](catcher: Catcher[F[A]])(
+      implicit monadError: MonadError[F, Throwable]) {
+    def cpsCatch(continuation: (F[A] => F[A]) => F[A]): F[A] = {
+      try {
+        continuation { fa: F[A] =>
+          monadError.handleErrorWith(fa) { e =>
+            catcher.applyOrElse(e, monadError.raiseError)
+          }
+        }
+      } catch {
+        case NonFatal(e) =>
+          catcher.applyOrElse(e, monadError.raiseError)
+      }
+    }
+  }
 
   implicit def implicitCatsFlatMap[F[_], A](fa: F[A]): CatsFlatMap[F, A] = CatsFlatMap(fa)
 
