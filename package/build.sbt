e2262ef6f52096726d8c91217f2a0c3bcead4bba
Yang Bo <atryyang@thoughtworks.com>
2018-02-24 03:40:30 +0800

    Add one more section

--- package/build.sbt
+++ package/build.sbt
@@ -3,3 +3,11 @@ enablePlugins(Example)
 publishArtifact := false
 
 libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.5.10"
+
+libraryDependencies += "org.scalaz" %% "scalaz-concurrent" % "7.2.19"
+
+libraryDependencies += "com.twitter" %% "algebird-core" % "0.13.4"
+
+libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.13.5"
+
+libraryDependencies += "com.thoughtworks.binding" %% "binding" % "11.0.1"
--- package/src/main/scala/com/thoughtworks/dsl/package.scala
+++ package/src/main/scala/com/thoughtworks/dsl/package.scala
@@ -21,17 +21,59 @@ package com.thoughtworks
   *
   * For example, the [[https://akka.io akka]] provides
   * [[https://doc.akka.io/docs/akka/2.5.10/fsm.html a DSL to create Finite State Machines]],
-  * which consists of some domain-specific instructions like [[akka.actor.AbstractFSM.when when]],
-  * [[akka.actor.AbstractFSM.goto goto]] and [[akka.actor.AbstractFSM.stay stay]].
+  * which consists of some domain-specific instructions like [[akka.actor.AbstractFSM#when when]],
+  * [[akka.actor.AbstractFSM#goto goto]] and [[akka.actor.AbstractFSM#stay stay]].
   * Unfortunately, you cannot embedded those instructions into your ordinary `if` / `while` / `try` control flows,
   * because Akka's DSL is required to be split into small closures,
   * preventing ordinary control flows from crossing the boundary of those closures.
   *
   * TensorFlow's [[https://www.tensorflow.org/api_guides/python/control_flow_ops control flow operations]] and
   * Caolan's [[https://github.com/caolan/async async]] library are examples of reinventing control flow
-  * in other languages.
+  * in languages other than Scala.
+  *
+  * == Monad: an interface of control flow ==
+  *
+  * It's too trivial to reinvent the whole set of control flow for each DSL.
+  * A simpler approach is only implementing a minimal interface required for control flows for each domain,
+  * while the syntax of other control flow operations are derived from the interface, shared between different domains.
+  *
+  * Since [[https://www.sciencedirect.com/science/article/pii/0890540191900524 computation can be represented as monads]],
+  * some libraries use monad as the interface of control flow,
+  * including [[scalaz.Monad]], [[cats.Monad]] and [[com.twitter.algebird.Monad]].
+  * A DSL author only have to implement two abstract method in [[scalaz.Monad]],
+  * and all the derived control flow operations
+  * like [[scalaz.syntax.MonadOps.whileM whileM]], [[scalaz.syntax.BindOps.ifM ifM]] are available.
+  * In addition, those monadic data type can be created and composed
+  * from Scala's built-in [[https://docs.scala-lang.org/tour/for-comprehensions.html `for` comprehension]].
+  *
+  * For example, you can use the same [[scalaz.syntax syntax]] to create [[org.scalacheck.Gen random value generators]]
+  * and [[com.thoughtworks.binding.Binding data-binding expressions]],
+  * as long as there are [[scalaz.Monad Monad]] instances
+  * for [[org.scalacheck.Gen]] and [[com.thoughtworks.binding.Binding]].
+  *
+  * Although the effort of creating a DSL is minimized with the help of monads,
+  * the syntax is still unsatisfactory.
+  * Methods in `MonadOps` still seem like reinventing control flow,
+  * and `for` comprehension supports only a limited set of functionality in comparison to ordinary control flows.
+  * `if` / `while` / `try` and other block expressions cannot appear in the enumerator clause of `for` comprehension.
+  *
+  * == Native control flows via monads ==
+  *
+  * == Continuation: The Mother of all Monads ==
+  *
+  * == Adaptive [[Dsl]] type class ==
   *
   * = Package Structure =
+  *
+  * @see [[https://www.schoolofhaskell.com/user/dpiponi/the-mother-of-all-monads The Mother of all Monads]]
+  * @see [[https://docs.scala-lang.org/tour/for-comprehensions.html `for` comprehension]]
+  * @see [[https://github.com/scala/scala-continuations scala-continuation]]
+  * @see [[https://github.com/scala/scala-async scala-async]]
+  * @see [[scalaz.syntax.MonadOps]],
+  * @see [[cats.syntax.MonadOps]],
+  * @see [[https://github.com/ThoughtWorksInc/each ThoughtWorks Each]]
+  * @see [[http://monadless.io/ Monadless]]
+  * @see [[https://github.com/pelotom/effectful effectful]]
   *
   */
 package object dsl
