2c750703e1723fd9bf8503b48434dadd6cd5f09e
杨博 (Yang Bo) <pop.atry@gmail.com>
2018-03-30 10:19:52 +0800

    Update Monadic.scala

--- keywords-Monadic/src/main/scala/com/thoughtworks/dsl/keywords/Monadic.scala
+++ keywords-Monadic/src/main/scala/com/thoughtworks/dsl/keywords/Monadic.scala
@@ -4,7 +4,10 @@ import com.thoughtworks.dsl.Dsl.Keyword
 import scala.language.higherKinds
 import scala.language.implicitConversions
 
-/**
+/** A keyword for creating monadic control flow.
+  *
+  * @see [[com.thoughtworks.dsl.domains.cats]] for using this [[Monadic]] keyword with [[cats.Monad]].
+  * @see [[com.thoughtworks.dsl.domains.scalaz]] for using this [[Monadic]] keyword with [[scalaz.Monad]].
   * @todo [[Monadic]] should be a [[scala.AnyVal]] after [[https://github.com/scala/bug/issues/10595]] is resolved.
   */
 final case class Monadic[F[*], A](fa: F[A]) extends Keyword[Monadic[F, A], A]
