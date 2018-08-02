af45bd328de70cbfdc9d7115c2e6c5af7f9d7d03
Yang Bo <atryyang@thoughtworks.com>
2018-08-02 21:48:42 +0800

    Simplify the test of "printf problem"

--- compilerplugins-BangNotation/src/test/scala/com/thoughtworks/dsl/compilerplugin/BangNotationSpec.scala
+++ compilerplugins-BangNotation/src/test/scala/com/thoughtworks/dsl/compilerplugin/BangNotationSpec.scala
@@ -1,6 +1,5 @@
 package com.thoughtworks.dsl.compilerplugin
 
-import com.thoughtworks.dsl.Dsl.reset
 import com.thoughtworks.dsl.Dsl.shift
 import org.scalatest.{FreeSpec, Matchers}
 
@@ -8,9 +7,21 @@ import org.scalatest.{FreeSpec, Matchers}
   * @author 杨博 (Yang Bo)
   */
 class BangNotationSpec extends FreeSpec with Matchers {
-  import BangNotationSpec._
 
-  "modifying answer type" in {
+  "printf problem" in {
+
+    object IntPlaceholder {
+      @shift def unary_! : String = ???
+      def cpsApply[Domain](f: String => Domain): Int => Domain = { i: Int =>
+        f(i.toString)
+      }
+    }
+
+    object StringPlaceholder {
+      @shift def unary_! : String = ???
+      def cpsApply[Domain](f: String => Domain): String => Domain = f
+    }
+
     def f1 = "Hello World!"
     def f2 = "Hello " + !StringPlaceholder + "!"
     def f3 = "The value of " + !StringPlaceholder + " is " + !IntPlaceholder + "."
@@ -18,30 +29,6 @@ class BangNotationSpec extends FreeSpec with Matchers {
     f1 should be("Hello World!")
     f2("World") should be("Hello World!")
     f3("x")(3) should be("The value of x is 3.")
-  }
-
-}
-
-object BangNotationSpec {
-
-  trait Placeholder[ParameterType] {
-    @shift
-    def unary_! : String = {
-      throw new RuntimeException("This method should be transformed to `cpsApply`.")
-    }
-
-    def cpsApply[Result](f: String => Result): ParameterType => Result = apply(f)
-    def apply[Result](f: String => Result): ParameterType => Result
-  }
-
-  object IntPlaceholder extends Placeholder[Int] {
-    def apply[Result](f: String => Result): Int => Result = { i: Int =>
-      f(i.toString)
-    }
-  }
-
-  object StringPlaceholder extends Placeholder[String] {
-    def apply[Result](f: String => Result): String => Result = f
   }
 
 }
