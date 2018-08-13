9082c5b4c1493c678f00166bc511bc94afa5bb12
Yang Bo <atryyang@thoughtworks.com>
2018-08-13 18:59:02 +0800

    Port Each to Scala 2.13

--- keywords-Each/src/main/scala/com/thoughtworks/dsl/keywords/Each.scala
+++ keywords-Each/src/main/scala/com/thoughtworks/dsl/keywords/Each.scala
@@ -1,12 +1,14 @@
 package com.thoughtworks.dsl.keywords
 
+import com.thoughtworks.enableMembersIf
 import com.thoughtworks.dsl.Dsl
 import com.thoughtworks.dsl.Dsl.{!!, Keyword}
 
-import scala.collection.GenTraversableOnce
-import scala.collection.generic.CanBuildFrom
+import scala.collection._
 import scala.language.implicitConversions
 import Shift.implicitShift
+
+import scala.collection.mutable.Builder
 
 /**
   * @author 杨博 (Yang Bo)
@@ -14,15 +16,53 @@ import Shift.implicitShift
 final case class Each[Element](elements: Traversable[Element]) extends Keyword[Each[Element], Element]
 object Each {
 
+  @enableMembersIf(scala.util.Properties.versionNumberString.matches("""^2\.1(1|2)\..*$"""))
+  private[Each] object Scala211Or212 {
+    type Factory[-A, +C] = scala.collection.generic.CanBuildFrom[Nothing, A, C]
+
+    @inline
+    def flatMapBreakOut[Element, Domain, DomainElement](
+        fa: Traversable[Element],
+        f: Element => GenTraversableOnce[DomainElement])(implicit factory: Factory[DomainElement, Domain]): Domain = {
+      fa.flatMap(f)(collection.breakOut(factory))
+    }
+
+    @inline
+    def newBuilder[A, C](implicit factory: Factory[A, C]): Builder[A, C] = {
+      factory()
+    }
+
+  }
+
+  @enableMembersIf(scala.util.Properties.versionNumberString.matches("""^2\.13\..*$"""))
+  private[Each] object Scala213 {
+
+    @inline
+    def flatMapBreakOut[Element, Domain, DomainElement](
+        fa: Traversable[Element],
+        f: Element => GenTraversableOnce[DomainElement])(implicit factory: Factory[DomainElement, Domain]): Domain = {
+      factory.fromSpecific(new View.FlatMap(fa, f))
+    }
+
+    @inline
+    def newBuilder[A, C](implicit factory: Factory[A, C]): Builder[A, C] = {
+      factory.newBuilder
+    }
+
+  }
+
+  import Scala211Or212._
+  import Scala213._
+
   implicit def implicitEach[Element](elements: Traversable[Element]): Each[Element] = Each[Element](elements)
 
   implicit def eachDsl[Element, Domain, DomainElement](
       implicit thatIsTraversableOnce: (Element => Domain) => (Element => GenTraversableOnce[DomainElement]),
-      bf: CanBuildFrom[Nothing, DomainElement, Domain]
+      factory: Factory[DomainElement, Domain]
   ): Dsl[Each[Element], Domain, Element] =
     new Dsl[Each[Element], Domain, Element] {
       def cpsApply(keyword: Each[Element], handler: Element => Domain): Domain = {
-        keyword.elements.flatMap(handler)(collection.breakOut(bf))
+        flatMapBreakOut(keyword.elements, handler)
       }
     }
 
@@ -37,14 +77,14 @@ object Each {
   implicit def continuationEachDsl[Element, LeftDomain, RightDomain, DomainElement](
       implicit rightDomainIsTraversableOnce: (Element => LeftDomain !! RightDomain) => (
           Element => LeftDomain !! TraversableOnce[DomainElement]),
-      bf: CanBuildFrom[Nothing, DomainElement, RightDomain],
+      factory: Factory[DomainElement, RightDomain],
       shiftDsl: Dsl[Shift[LeftDomain, TraversableOnce[DomainElement]], LeftDomain, TraversableOnce[DomainElement]]
   ): Dsl[Each[Element], LeftDomain !! RightDomain, Element] = {
     new Dsl[Each[Element], LeftDomain !! RightDomain, Element] {
       def cpsApply(keyword: Each[Element],
-                    handler0: Element => LeftDomain !! RightDomain): LeftDomain !! RightDomain = {
+                   handler0: Element => LeftDomain !! RightDomain): LeftDomain !! RightDomain = {
         val i = keyword.elements.toIterator
-        val builder = bf()
+        val builder = newBuilder[DomainElement, RightDomain]
         val handler = rightDomainIsTraversableOnce(handler0)
         @inline
         def loop(continue: RightDomain => LeftDomain): LeftDomain = {
