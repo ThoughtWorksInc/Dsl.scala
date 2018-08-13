180785eb3c7f181110b746218f0032e97174deaa
Yang Bo <atryyang@thoughtworks.com>
2018-08-13 19:16:46 +0800

    Port Fork to Scala 2.13

--- keywords-Fork/src/main/scala/com/thoughtworks/dsl/keywords/Fork.scala
+++ keywords-Fork/src/main/scala/com/thoughtworks/dsl/keywords/Fork.scala
@@ -6,15 +6,55 @@ import java.util.concurrent.atomic.AtomicInteger
 import com.thoughtworks.dsl.Dsl
 import com.thoughtworks.dsl.Dsl.{!!, Keyword}
 import com.thoughtworks.dsl.keywords.Catch.CatchDsl
+import com.thoughtworks.enableMembersIf
 
-import scala.collection.{GenTraversableOnce, mutable}
+import scala.collection._
 import scala.collection.generic.CanBuildFrom
-import scala.util.Try
+import scala.collection.mutable.Builder
 import scala.language.implicitConversions
 
 final case class Fork[Element](elements: Traversable[Element]) extends AnyVal with Keyword[Fork[Element], Element]
 
 object Fork {
+
+  @enableMembersIf(scala.util.Properties.versionNumberString.matches("""^2\.1(1|2)\..*$"""))
+  private[Fork] object Scala211Or212 {
+    type Factory[-A, +C] = scala.collection.generic.CanBuildFrom[Nothing, A, C]
+
+    @inline
+    def flatMapBreakOut[Element, Domain, DomainElement](
+        fa: Traversable[Element],
+        f: Element => GenTraversableOnce[DomainElement])(implicit factory: Factory[DomainElement, Domain]): Domain = {
+      fa.flatMap(f)(collection.breakOut(factory))
+    }
+
+    @inline
+    def newBuilder[A, C](implicit factory: Factory[A, C]): Builder[A, C] = {
+      factory()
+    }
+
+  }
+
+  @enableMembersIf(scala.util.Properties.versionNumberString.matches("""^2\.13\..*$"""))
+  private[Fork] object Scala213 {
+
+    @inline
+    def flatMapBreakOut[Element, Domain, DomainElement](
+        fa: Traversable[Element],
+        f: Element => GenTraversableOnce[DomainElement])(implicit factory: Factory[DomainElement, Domain]): Domain = {
+      factory.fromSpecific(new View.FlatMap(fa, f))
+    }
+
+    @inline
+    def newBuilder[A, C](implicit factory: Factory[A, C]): Builder[A, C] = {
+      factory.newBuilder
+    }
+
+  }
+
+  import Scala211Or212._
+  import Scala213._
+
   implicit def implicitFork[Element](elements: Traversable[Element]): Fork[Element] = Fork[Element](elements)
 
   final case class MultipleException(throwableSet: Set[Throwable])
@@ -42,7 +82,7 @@ object Fork {
     override def getStackTrace: Array[StackTraceElement] = synchronized {
       super.getStackTrace match {
         case null =>
-          setStackTrace(throwableSet.flatMap(_.getStackTrace)(collection.breakOut))
+          setStackTrace(throwableSet.view.flatMap(_.getStackTrace).toArray)
           super.getStackTrace
         case stackTrace =>
           stackTrace
@@ -59,14 +99,14 @@ object Fork {
       implicit eachDsl: Dsl[Each[NarrowElement], LeftDomain, NarrowElement],
       booleanEachDsl: Dsl[Each[Boolean], LeftDomain, Boolean],
       isTraversableOnce: RightDomain => TraversableOnce[WidenElement],
-      canBuildFrom: CanBuildFrom[Nothing, WidenElement, RightDomain],
+      canBuildFrom: Factory[WidenElement, RightDomain],
       breakDsl: Dsl[Break, LeftDomain, Nothing],
       catchDsl: CatchDsl[LeftDomain, LeftDomain, Unit]
   ): Dsl[Fork[NarrowElement], LeftDomain !! RightDomain, NarrowElement] =
     new Dsl[Fork[NarrowElement], LeftDomain !! RightDomain, NarrowElement] {
       def cpsApply(fork: Fork[NarrowElement],
-                    mapper: NarrowElement => LeftDomain !! RightDomain): LeftDomain !! RightDomain = _ {
-        val builder: mutable.Builder[WidenElement, RightDomain] = canBuildFrom()
+                   mapper: NarrowElement => LeftDomain !! RightDomain): LeftDomain !! RightDomain = _ {
+        val builder: mutable.Builder[WidenElement, RightDomain] = newBuilder[WidenElement, RightDomain]
         val exceptionBuilder = Set.newBuilder[Throwable]
         val counter = new AtomicInteger(1)
         if (!Each(Seq(true, false))) {
