8ba24f9ace34c97a961e96b6bacf8870c2e0de7a
Yang Bo <atryyang@thoughtworks.com>
2018-08-25 20:08:30 +0800

    Remove unused comments

--- keywords-Get/src/main/scala/com/thoughtworks/dsl/keywords/Get.scala
+++ keywords-Get/src/main/scala/com/thoughtworks/dsl/keywords/Get.scala
@@ -16,29 +16,4 @@ object Get {
     }
   }
 
-//  implicit def getDsl[LeftDomain, Value, RightDomain]: Dsl[Get[Value], LeftDomain !! Value !! RightDomain, Value] =
-//    new Dsl[Get[Value], LeftDomain !! Value !! RightDomain, Value] {
-//      def cpsApply(keyword: Get[Value],
-//                   handler: Value => !![!![LeftDomain, Value], RightDomain]): !![!![LeftDomain, Value], RightDomain] = {
-//        (k0: RightDomain => !![LeftDomain, Value]) => (k1: Value => LeftDomain) =>
-//
-//          ???
-//      }
-//    }
-
-//  implicit def getDsl[Domain, Value]: Dsl[Get[Value], Value !! Domain, Value] =
-//    new Dsl[Get[Value], Value !! Domain, Value] {
-//      def cpsApply(keyword: Get[Value], handler: Value => !![Value, Domain]): !![Value, Domain] = { (k: Domain => Value) =>
-//        handler(k(???))(k)
-////        handler { (k: Domain => Value) =>
-////          ??? : Value
-////        }(k)
-//      }
-//    }
-//    new Dsl[Get[Value], Domain !! Value, Value] {
-//      def cpsApply(keyword: Get[Value], handler: Value => (Domain !! Value)): Domain !! Value = { (k: Value => Domain) =>
-//        ??? : Domain
-//      }
-//
-//    }
 }
