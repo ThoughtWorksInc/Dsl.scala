22dcd82d757ea7cd28d14024147c9d5ea0a96791
Yang Bo <atryyang@thoughtworks.com>
2018-09-06 09:01:25 +0800

    Add Yield.From keyword

--- keywords-Yield/src/main/scala/com/thoughtworks/dsl/keywords/Yield.scala
+++ keywords-Yield/src/main/scala/com/thoughtworks/dsl/keywords/Yield.scala
@@ -20,19 +20,41 @@ final case class Yield[Element](element: Element) extends AnyVal with Keyword[Yi
 
 object Yield {
 
+  final case class From[Element](elements: Stream[Element]) extends AnyVal with Keyword[From[Element], Unit]
+
+  def apply[Element](element0: Element, element1: Element, restElements: Element*) = {
+    From(new Stream.Cons(element0, new Stream.Cons(element1, restElements.toStream)))
+  }
+
+  implicit def implicitYieldFrom[Element](elements: Stream[Element]): From[Element] = From[Element](elements)
+
+  implicit def yieldFromDsl[Element, That >: Element]: Dsl[From[Element], Stream[That], Unit] =
+    new Dsl[From[Element], Stream[That], Unit] {
+      def cpsApply(keyword: From[Element], generateTail: Unit => Stream[That]): Stream[That] = {
+        keyword.elements.append(generateTail(()))
+      }
+    }
+
+  implicit def futureYieldFromDsl[Element, That >: Element]: Dsl[From[Element], Stream[Future[That]], Unit] =
+    new Dsl[From[Element], Stream[Future[That]], Unit] {
+      def cpsApply(keyword: From[Element], generateTail: Unit => Stream[Future[That]]): Stream[Future[That]] = {
+        keyword.elements.map(Future.successful).append(generateTail(()))
+      }
+    }
+
   implicit def implicitYield[Element](element: Element): Yield[Element] = Yield[Element](element)
 
   implicit def yieldDsl[Element, That >: Element]: Dsl[Yield[Element], Stream[That], Unit] =
     new Dsl[Yield[Element], Stream[That], Unit] {
-      def cpsApply(keyword: Yield[Element], mapper: Unit => Stream[That]): Stream[That] = {
-        new Stream.Cons(keyword.element, mapper(()))
+      def cpsApply(keyword: Yield[Element], generateTail: Unit => Stream[That]): Stream[That] = {
+        new Stream.Cons(keyword.element, generateTail(()))
       }
     }
 
   implicit def futureYieldDsl[Element, That >: Element]: Dsl[Yield[Element], Stream[Future[That]], Unit] =
     new Dsl[Yield[Element], Stream[Future[That]], Unit] {
-      def cpsApply(keyword: Yield[Element], handler: Unit => Stream[Future[That]]): Stream[Future[That]] = {
-        new Stream.Cons(Future.successful(keyword.element), handler(()))
+      def cpsApply(keyword: Yield[Element], generateTail: Unit => Stream[Future[That]]): Stream[Future[That]] = {
+        new Stream.Cons(Future.successful(keyword.element), generateTail(()))
       }
     }
 }
