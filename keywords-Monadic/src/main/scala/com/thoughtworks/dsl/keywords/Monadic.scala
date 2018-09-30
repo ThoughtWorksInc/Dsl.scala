6a64e380358925f4804d6e2db7caa8e77d1a9dac
Yang Bo <atryyang@thoughtworks.com>
2018-09-30 21:08:55 +0800

    Improve Scaladoc

--- keywords-Monadic/src/main/scala/com/thoughtworks/dsl/keywords/Monadic.scala
+++ keywords-Monadic/src/main/scala/com/thoughtworks/dsl/keywords/Monadic.scala
@@ -4,14 +4,14 @@ import com.thoughtworks.dsl.Dsl.Keyword
 import scala.language.higherKinds
 import scala.language.implicitConversions
 
-/** A keyword for creating monadic control flow.
+/** A keyword for extracting monadic value from the monadic expression [[fa]].
   *
   * @see [[com.thoughtworks.dsl.domains.cats]] for using this [[Monadic]] keyword with [[cats.Monad]].
   * @see [[com.thoughtworks.dsl.domains.scalaz]] for using this [[Monadic]] keyword with [[scalaz.Monad]].
   * @todo [[Monadic]] should be a [[scala.AnyVal]] after [[https://github.com/scala/bug/issues/10595]] is resolved.
   */
-final case class Monadic[F[*], A](fa: F[A]) extends Keyword[Monadic[F, A], A]
+final case class Monadic[F[_], A](fa: F[A]) extends Keyword[Monadic[F, A], A]
 
 object Monadic {
-  implicit def implicitMonadic[F[*], A](fa: F[A]): Monadic[F, A] = Monadic(fa)
+  implicit def implicitMonadic[F[_], A](fa: F[A]): Monadic[F, A] = Monadic(fa)
 }
