234f5f7d64ed2b757d2e8389a8205c654197fa21
Yang, Bo <pop.atry@gmail.com>
2021-11-14 11:46:19 -0800

    Update YieldSpec213.scala

--- keywords-Yield/src/test/scala-2.13/com/thoughtworks/dsl/keywords/YieldSpec213.scala
+++ keywords-Yield/src/test/scala-2.13/com/thoughtworks/dsl/keywords/YieldSpec213.scala
@@ -3,7 +3,7 @@ package com.thoughtworks.dsl.keywords
 import com.thoughtworks.dsl.Dsl.!!
 import com.thoughtworks.enableMembersIf
 import org.scalatest.freespec.AnyFreeSpec
-import org.scalatest.matchers.Matchers
+import org.scalatest.matchers.should.Matchers
 
 import scala.annotation.tailrec
 import scala.collection.{LinearSeq, SeqView}
