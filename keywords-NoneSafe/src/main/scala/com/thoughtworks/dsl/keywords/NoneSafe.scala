60d066e8dc2d9271c0932c75f8dcab63279bb619
Yang, Bo <pop.atry@gmail.com>
2021-11-27 11:47:36 -0800

    Fix compilation for NoneSafe

--- keywords-NoneSafe/src/main/scala/com/thoughtworks/dsl/keywords/NoneSafe.scala
+++ keywords-NoneSafe/src/main/scala/com/thoughtworks/dsl/keywords/NoneSafe.scala
@@ -1,11 +1,12 @@
 package com.thoughtworks.dsl
 package keywords
-import com.thoughtworks.dsl.Dsl.Keyword
+import com.thoughtworks.dsl.Dsl.IsKeyword
 import scala.language.implicitConversions
 
-final case class NoneSafe[A](option: Option[A]) extends AnyVal with Keyword[NoneSafe[A], A]
+final case class NoneSafe[A](option: Option[A]) extends AnyVal
 
 object NoneSafe {
+  given [A]: IsKeyword[NoneSafe[A], A] with {}
 
   implicit def noneSafeDsl[A, Domain](implicit
       continueDsl: Dsl[Return[None.type], Domain, Nothing]
