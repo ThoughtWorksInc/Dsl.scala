17362fc7460b3eeb5a5192cb08c212589693a03d
Yang, Bo <pop.atry@gmail.com>
2021-11-27 10:58:27 -0800

    Fix compilation errors in Get.scala

--- keywords-Get/src/main/scala/com/thoughtworks/dsl/keywords/Get.scala
+++ keywords-Get/src/main/scala/com/thoughtworks/dsl/keywords/Get.scala
@@ -1,15 +1,18 @@
 package com.thoughtworks.dsl.keywords
 import com.thoughtworks.dsl.Dsl
-import com.thoughtworks.dsl.Dsl.Keyword
+import com.thoughtworks.dsl.Dsl.IsKeyword
 
-/** @see [[Put]]
-  * @author 杨博 (Yang Bo)
+/** @see
+  *   [[Put]]
+  * @author
+  *   杨博 (Yang Bo)
   */
-final case class Get[S]() extends Keyword[Get[S], S]
+final case class Get[S]()
 
 object Get {
+  given [S]: IsKeyword[Get[S], S] with {}
 
-  implicit def getDsl[S0, S <: S0, A] = new Dsl[Get[S0], S => A, S0] {
+  implicit def getDsl[S0, S <: S0, A]: Dsl[Get[S0], S => A, S0] = new Dsl[Get[S0], S => A, S0] {
     def cpsApply(keyword: Get[S0], handler: S0 => S => A): S => A = { b =>
       handler(b)(b)
     }
