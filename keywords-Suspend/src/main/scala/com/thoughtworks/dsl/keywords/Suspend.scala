d97e1312b528a3f93012f24732a461cac8a19130
Yang, Bo <pop.atry@gmail.com>
2021-11-27 11:38:49 -0800

    Let Dsl for Suspend have lower priority than Dsl.Derived

--- keywords-Suspend/src/main/scala/com/thoughtworks/dsl/keywords/Suspend.scala
+++ keywords-Suspend/src/main/scala/com/thoughtworks/dsl/keywords/Suspend.scala
@@ -11,7 +11,7 @@ object Suspend {
 
   given[Upstream, UpstreamValue](using upstreamIsKeyword: => IsKeyword[Upstream, UpstreamValue]): IsKeyword[Suspend[Upstream], UpstreamValue] with {}
 
-  given[Keyword, Domain, Value](using Dsl[Keyword, Domain, Value]): Dsl[Suspend[Keyword], Domain, Value] with {
+  given[Keyword, Domain, Value](using util.NotGiven[Dsl.Derived[Suspend[Keyword], Domain, Value]], Dsl[Keyword, Domain, Value]): Dsl[Suspend[Keyword], Domain, Value] with {
     def cpsApply(keyword: Suspend[Keyword], handler: Value => Domain): Domain = {
       keyword().cpsApply(handler)          
     }
