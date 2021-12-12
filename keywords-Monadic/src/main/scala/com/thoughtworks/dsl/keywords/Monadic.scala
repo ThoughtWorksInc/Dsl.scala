7d5b2b87ff35b5c9b3e5d4d4796370ae28bc3333
Yang, Bo <pop.atry@gmail.com>
2021-12-12 13:27:53 -0800

    Create Monadic.unary_! instead of Monadic.implicitMonadic

--- keywords-Monadic/src/main/scala/com/thoughtworks/dsl/keywords/Monadic.scala
+++ keywords-Monadic/src/main/scala/com/thoughtworks/dsl/keywords/Monadic.scala
@@ -1,7 +1,6 @@
 package com.thoughtworks.dsl
 package keywords
 import Dsl.AsKeyword
-import Dsl.AsKeyword
 import scala.language.higherKinds
 import scala.language.implicitConversions
 
@@ -14,13 +13,21 @@ import scala.language.implicitConversions
   * @todo
   *   [[Monadic]] should be a [[scala.AnyVal]] after [[https://github.com/scala/bug/issues/10595]] is resolved.
   */
-opaque type Monadic[Functor[_], Value] <: Dsl.Keyword.Opaque = Dsl.Keyword.Opaque.Of[Functor[Value]]
+opaque type Monadic[Functor[_], Value] <: Dsl.Keyword.Opaque =
+  Dsl.Keyword.Opaque.Of[Functor[Value]]
 
 object Monadic {
+
+  extension [FA, F[_], A](inline fa: FA)(using
+      inline notKeyword: util.NotGiven[
+        FA <:< Dsl.Keyword
+      ],
+      inline asFA: FA <:< F[A]
+  )
+    transparent inline def unary_! : A =
+      Dsl.shift[Monadic[F, A], A](Monadic[F, A](asFA(fa))): A
+
   @inline def apply[Functor[_], Value]: Functor[Value] =:= Monadic[Functor, Value] = Dsl.Keyword.Opaque.Of.apply
 
   given [Functor[_], Value]: AsKeyword.IsKeyword[Monadic[Functor, Value], Value] with {}
-
-  given implicitMonadic[Functor[_], Value]: AsKeyword[Functor[Value], Monadic[Functor, Value], Value] = Monadic(_)
-
 }
