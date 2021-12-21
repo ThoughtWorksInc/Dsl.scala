43d95d19b54d3d6b4155fc8752a892dd9c9c1955
Yang, Bo <pop.atry@gmail.com>
2021-12-20 22:36:43 -0800

    Add tests

--- reset/src/test/scala/com/thoughtworks/dsl/keywords/ReturnSpec.scala
+++ reset/src/test/scala/com/thoughtworks/dsl/keywords/ReturnSpec.scala
@@ -6,6 +6,7 @@ import utest.{TestSuite, Tests, given}
 import Dsl.Run
 import scala.language.implicitConversions
 import Dsl.IsKeyword
+import scala.util.control.NonFatal
 
 /**
   * @author 杨博 (Yang Bo)
@@ -100,6 +101,35 @@ object ReturnSpec extends TestSuite {
         }
 
         assert(continuation(identity) == "my string")
+      }
+      "match / case " - {
+        val continuation = *[[X] =>> AnyRef !! X] {
+          val b = true
+          val c = "my string"
+          val d = new StringBuilder
+          b match {
+            case true =>
+              "my string"
+            case false =>
+              d
+          }
+        }
+
+        assert(continuation(identity) == "my string")
+      }
+
+      "try / catch" - {
+        val continuation = *[[X] =>> Unit !! Throwable !! X] {
+          val c = "my string"
+          val d = new StringBuilder
+          try {
+            c
+          } catch {
+            case NonFatal(e) =>
+              d
+          }
+        }
+
       }
     }
 
