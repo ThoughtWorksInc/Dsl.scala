931aab6e19bd354a43b295000db939742f2f57c1
Yang, Bo <pop.atry@gmail.com>
2021-12-20 22:36:43 -0800

    Add IsKeyword instance for Match

--- keywords-Match/src/main/scala/com/thoughtworks/dsl/keywords/Match.scala
+++ keywords-Match/src/main/scala/com/thoughtworks/dsl/keywords/Match.scala
@@ -9,45 +9,66 @@ object Match {
 
   opaque type +:[+A, +B] = A | B
 
-  given [
-      Index <: Int,
-      Keyword,
-      Domain,
-      LastValue
-  ](using
-      dsl: Dsl.PolyCont[Keyword, Domain, LastValue],
-      valueOfIndex: ValueOf[Index]
-  ): Dsl.PolyCont[WithIndex[Index, Keyword] +: Nothing, Domain, LastValue] with {
-    def cpsApply(keywordWithIndex: WithIndex[Index, Keyword] +: Nothing, handler: LastValue => Domain): Domain = {
-      keywordWithIndex match {
-        case WithIndex(valueOfIndex.value, keyword) =>
-          dsl.cpsApply(keyword, handler)
-        case _ =>
-          throw new IllegalArgumentException("Invalid index")
+  object WithIndex:
+    
+    given [
+        Index <: Int,
+        Keyword,
+        Domain,
+        LastValue
+    ](using
+        dsl: Dsl.IsKeyword[Keyword, LastValue],
+        valueOfIndex: ValueOf[Index]
+    ): Dsl.IsKeyword[WithIndex[Index, Keyword] +: Nothing, LastValue] with {} 
+    given [
+        Index <: Int,
+        Keyword,
+        Domain,
+        LastValue
+    ](using
+        dsl: Dsl.PolyCont[Keyword, Domain, LastValue],
+        valueOfIndex: ValueOf[Index]
+    ): Dsl.PolyCont[WithIndex[Index, Keyword] +: Nothing, Domain, LastValue] with {
+      def cpsApply(keywordWithIndex: WithIndex[Index, Keyword] +: Nothing, handler: LastValue => Domain): Domain = {
+        keywordWithIndex match {
+          case WithIndex(valueOfIndex.value, keyword) =>
+            dsl.cpsApply(keyword, handler)
+          case _ =>
+            throw new IllegalArgumentException("Invalid index")
+        }
       }
     }
-  }
 
-  given [
-      Index <: Int,
-      LeftKeyword,
-      RestKeyword,
-      Domain,
-      Value
-  ](using
-      leftDsl: Dsl.PolyCont[LeftKeyword, Domain, Value],
-      valueOfIndex: ValueOf[Index],
-      restDsl: Dsl.PolyCont[RestKeyword, Domain, Value]
-  ): Dsl.PolyCont[WithIndex[Index, LeftKeyword] +: RestKeyword, Domain, Value] with {
-    def cpsApply(keywordUnion: WithIndex[Index, LeftKeyword] +: RestKeyword, handler: Value => Domain): Domain = {
-      keywordUnion match {
-        case WithIndex(valueOfIndex.value, leftKeyword) =>
-          leftDsl.cpsApply(leftKeyword.asInstanceOf[LeftKeyword], handler)
-        case _ =>
-          restDsl.cpsApply(keywordUnion.asInstanceOf[RestKeyword], handler)
+    given [
+        Index <: Int,
+        LeftKeyword,
+        RestKeyword,
+        Domain,
+        Value
+    ](using
+        leftDsl: Dsl.IsKeyword[LeftKeyword, Value],
+        restDsl: Dsl.IsKeyword[RestKeyword, Value]
+    ): Dsl.IsKeyword[WithIndex[Index, LeftKeyword] +: RestKeyword, Value] with {}
+    given [
+        Index <: Int,
+        LeftKeyword,
+        RestKeyword,
+        Domain,
+        Value
+    ](using
+        leftDsl: Dsl.PolyCont[LeftKeyword, Domain, Value],
+        valueOfIndex: ValueOf[Index],
+        restDsl: Dsl.PolyCont[RestKeyword, Domain, Value]
+    ): Dsl.PolyCont[WithIndex[Index, LeftKeyword] +: RestKeyword, Domain, Value] with {
+      def cpsApply(keywordUnion: WithIndex[Index, LeftKeyword] +: RestKeyword, handler: Value => Domain): Domain = {
+        keywordUnion match {
+          case WithIndex(valueOfIndex.value, leftKeyword) =>
+            leftDsl.cpsApply(leftKeyword.asInstanceOf[LeftKeyword], handler)
+          case _ =>
+            restDsl.cpsApply(keywordUnion.asInstanceOf[RestKeyword], handler)
+        }
       }
     }
-  }
 
 }
 // @inline def Match = keywords.FlatMap
